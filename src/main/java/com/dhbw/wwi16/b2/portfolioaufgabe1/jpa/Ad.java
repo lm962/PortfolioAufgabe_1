50d0072af3bfdd01eaf613db11388c9ed1002106
geizzo <geizzo@ABN0200.abilis.lan>
2018-02-28 10:09:15 +0100

    Ad-Class added

---
+++ src/main/java/com/dhbw/wwi16/b2/portfolioaufgabe1/jpa/Ad.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.dhbw.wwi16.b2.portfolioaufgabe1.jpa;
+
+import java.io.Serializable;
+import java.sql.Date;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
+import javax.persistence.*;
+import lombok.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ *
+ * @author geizzo
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Ad implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;   
+    
+    @ManyToOne
+    private User user;
+    
+    @ManyToOne
+    private Category category;
+    
+    @Enumerated(EnumType.STRING)
+    private Offertype offertype;
+    
+    private String title;
+    
+    @Lob
+    private String description;
+    
+    private Date creationdate;
+    
+    private double price;
+    
+    @Enumerated(EnumType.STRING)
+    private Pricetype pricetype; 
+    
+    
+    private enum Offertype{
+        Biete, Suche 
+    }
+    private enum Pricetype{
+        Verhandlungsbasis, Festpreis
+    }
+    
+    
+}
